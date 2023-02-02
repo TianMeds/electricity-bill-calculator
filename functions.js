285f47f5b1b73adcfc3984a09ec0b7e7a62244a8
jbberbon <jbberbon@student.apc.edu.ph>
2023-02-03 01:34:55 +0800

    Feb 03 - Running but no dynamic total. and buggy and very messy code

--- functions.js
+++ functions.js
@@ -1,78 +1,125 @@
-//Object 
-function appliance(name, rate, watt, usage, {computedCost}) {
-    this.name = name;
-    this.rate = rate;
-    this.watt = watt;
-    this.usage = usage;
-    this.computedCost = computedCost;
-}
-
-
-export const computedResult = () => {
-    //Variable Assignment from form inputs
-    let rate = document.getElementById('rate').value,
-        name = document.getElementById('applianceName').value,
-        watt = document.getElementById('watt').value,
-        usage = document.getElementById('usage').value;
-
-    //Calculation of electricity cost
-    const hourly = hourlyCost(watt, rate);
-    const daily = dailyCost(watt, usage, rate);
-    const monthly = monthlyCost(watt, usage, rate);
-
-    return {name, hourly, daily, monthly};
-}
-
 //Math formulae
-export const hourlyCost = (watt, rate) => {
-    return (watt / 1000) * rate;
-}
-export const dailyCost = (watt, usage, rate) => {
-    return ((watt * usage) / 1000) * rate;
-}
-export const monthlyCost = (watt, usage, rate) => {
-    return (((watt * usage * 30)) / 1000) * rate;
-}
+const hourly_cost = (watt, rate) => {
+    var hourly = (watt / 1000) * rate
+    return hourly;
+};
+const daily_cost = (watt, usage, rate) => {
+    let daily = ((watt * usage) / 1000) * rate;
+    return daily;
+};
+const monthly_cost = (watt, usage, rate) => {
+    let monthly = (((watt * usage * 30)) / 1000) * rate;
+    return monthly;
+};
 
+// var form_data = {};
+export const retrieve_form_data = () => {
+    var form_data = {};
+    form_data["name"] = document.getElementById('applianceName').value;
+    form_data["rate"] = document.getElementById('rate').value;
+    form_data["watt"] = document.getElementById('watt').value;
+    form_data["usage"] = document.getElementById('usage').value;
 
+    return form_data; 
+};
+export const compute_form_data = (retrieved_data) => {
+    var computed_data = {};
+    computed_data["name"] = retrieved_data.name;
+    computed_data["hourly"] = (hourly_cost(retrieved_data.watt, retrieved_data.rate)).toFixed(2); 
+    computed_data["daily"] = (daily_cost(retrieved_data.watt, retrieved_data.usage, retrieved_data.rate)).toFixed(2);
+    computed_data["monthly"] = (monthly_cost(retrieved_data.watt, retrieved_data.usage, retrieved_data.rate)).toFixed(2);
 
-export const populateTable = (result) => {
-    const table = document.querySelector('#dataResult');
-    let row = document.createElement('tr');
+    return computed_data;
+};
 
-    Object.values(result).forEach(data => {
-        let cell = document.createElement('td');
-        let textNode = document.createTextNode(data);
-        cell.appendChild(textNode);
+//Creation and Append of Table Headers
+export const append_table_header = () => {
+    var table = document.createElement("table");
+    table.setAttribute('id', 'data-result');
+    var thead = document.createElement("thead");
+    var header_row = document.createElement("tr");
+
+    var header = ['Appliance', 'Hourly', 'Daily', 'Monthly', 'Actions'];
+    header.forEach(headers => {
+        var header_cell = document.createElement("th");
+        header_cell.classList.add("table-header");
+        header_cell.textContent = headers;
+        header_row.appendChild(header_cell);
+    });
+    thead.appendChild(header_row);
+    table.appendChild(thead);
+    document.querySelector("#result").appendChild(table);
+};
+
+export const populate_table = (computed_data) => {
+    const table = document.querySelector('#data-result');
+    const row = document.createElement('tr');
+    
+    Object.values(computed_data).forEach(data => {
+        const cell = document.createElement('td'); //New individual table data tag
+        cell.classList.add('table-data'); //Add class to td tag
+        cell.textContent = data;
         row.appendChild(cell);
-    })
-    document.querySelector('#dataResult').appendChild(row);
+    });
+    
+    const button_cell = document.createElement('td');
+    const edit_button = document.createElement('button');
+    edit_button.textContent = 'Edit';
+    edit_button.classList.add('edit');
+    const delete_button = document.createElement('button');
+    delete_button.textContent = 'Delete';
+    delete_button.classList.add('delete');
+    button_cell.appendChild(edit_button);
+    button_cell.appendChild(delete_button);
+    row.appendChild(button_cell)
+
+    table.appendChild(row);
+};
+
+export const update_record = (computed_data, row) => {
+    row.cells[0].innerHTML = computed_data.name;
+    row.cells[1].innerHTML = computed_data.hourly;
+    row.cells[2].innerHTML = computed_data.daily;
+    row.cells[3].innerHTML = computed_data.monthly;
+};
+
+export const reset_form = () => {
+    document.querySelector('form').reset(); //Clear form for the next entry
+    const row = null; 
+
+    return row;
 }
 
-// export const populateTable = (resultsArray) => {
-//     const table = document.querySelector('#dataResult');
-//     let row = document.createElement('tr');
-//     resultsArray.forEach(object => {
-//         Object.values(object).forEach(data => {
-//             let cell = document.createElement('td');
-//             let textNode = document.createTextNode(data);
-//             cell.appendChild(textNode);
-//             row.appendChild(cell);
-//         })
-//     })
-//     document.querySelector('#dataResult').appendChild(row);
-// }
+/*
+export const on_edit = (array_of_form_data, callback) => {
+    let table = document.getElementById("data-result");
+    let edit_buttons = table.querySelectorAll(".edit");
+    for (let i = 0; i < edit_buttons.length; i++) {
+        let edit_button = edit_buttons[i];
+        edit_button.addEventListener("click", function() {
+            let row_index = edit_button.parentElement.parentElement.rowIndex - 1;
+            let row = edit_button.parentNode.parentNode;
+            document.querySelector("#rate").value = array_of_form_data[i].rate;
+            document.querySelector("#applianceName").value = array_of_form_data[i].name;
+            document.querySelector("#watt").value = array_of_form_data[i].watt;
+            document.querySelector("#usage").value = array_of_form_data[i].usage;
 
-//-------------------------------------------------------------------------------------//
-
-//UNUSED//
-export const appendText = (element, value) => {
-    const addedText = document.createTextNode(value); //stores function parameter to a variable
-    const result = document.querySelector("." + element); //stores desired element id
-
-    result.appendChild(addedText); //Appends text to selected element class/id
+            callback(row, row_index);
+        });
+    } 
 }
-export const insertAfter = (referenceNode, newNode) => {
-    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-}
-
+export const on_delete = (row) => {
+    let table = document.getElementById("data-result");
+    let delete_buttons = table.querySelectorAll(".delete");
+    for (let i = 0; i < delete_buttons.length; i++) {
+        let delete_button = delete_buttons[i];
+        delete_button.addEventListener("click", function() {
+            if (confirm('Do you want to delete this record?')) {
+                row = delete_button.parentElement.parentElement;
+                document.getElementById('data-result').deleteRow(row.rowIndex);
+                row = reset_form();
+            }
+        });
+    } 
+} 
+*/
--- script.js
+++ script.js
@@ -1,27 +1,75 @@
-import {insertAfter, populateTable, computedResult} from './functions.js';
+import {
+    append_table_header,
+    populate_table,
+    compute_form_data,
+    retrieve_form_data,
+    update_record,
+    reset_form
+} from './functions.js';
 
-const form = document.querySelector('#userInput'); //storing form data into a const
+const form = document.querySelector('#userInput'); 
+const array_of_form_data = [];
+const computed_data_array = [];
+let function_has_run = false;
+let row_index = 0;
+let row = null;
 
-const resultArray = [];
 
-const calculate = (ev)=> { //Function expression && arrow function
+form.addEventListener('submit', (ev) => {
     ev.preventDefault(); //Stop the form from submitting & stop refreshing page
+    if (row == null) {
+        if (!function_has_run) { // Makes sure table header will output once
+            append_table_header();
+            function_has_run = true;
+        }  
+        let retrieved_data = retrieve_form_data();
+        array_of_form_data.push(retrieved_data); //Store form values to array of objects outside this function
 
-    //variable assignment from form and calculation
-    const result = computedResult();
-    //Appends object to array
-    resultArray.push(result);
-    populateTable(result);
-
-    console.log(resultArray);
-
-    document.querySelector('form').reset(); //Clear form for the next entries
-}
-
-form.addEventListener('submit', calculate); //When button is clicked, calculate() gets executed 
-
-
+        let computed_data = compute_form_data(retrieved_data);
+        computed_data_array.push(computed_data);
+    
+        //Populates table rows 
+        populate_table(computed_data);
 
 
-// -------------------------------------------------------------------------------------------------------------------- //
+        let table = document.getElementById("data-result");
+        let edit_buttons = table.querySelectorAll(".edit");
+        for (let i = 0; i < edit_buttons.length; i++) {
+            let edit_button = edit_buttons[i];
+            edit_button.addEventListener("click", function() {
+                row_index = edit_button.parentElement.parentElement.rowIndex - 1;
+                row = edit_button.parentNode.parentNode;
+                document.querySelector("#rate").value = array_of_form_data[i].rate;
+                document.querySelector("#applianceName").value = array_of_form_data[i].name;
+                document.querySelector("#watt").value = array_of_form_data[i].watt;
+                document.querySelector("#usage").value = array_of_form_data[i].usage;
+            });
+        } 
+        
+        let delete_buttons = table.querySelectorAll(".delete");
+        for (let i = 0; i < delete_buttons.length; i++) {
+            let delete_button = delete_buttons[i];
+            delete_button.addEventListener("click", function() {
+                if (confirm('Do you want to delete this record?')) {
+                    row = delete_button.parentElement.parentElement;
+                    document.getElementById('data-result').deleteRow(row.rowIndex);
+                    row = reset_form();
+                }
+            });
+        } 
+        
+    }
+    else {
+        let retrieved_data = retrieve_form_data();
+        array_of_form_data[row_index] = retrieved_data;
+        let computed_data = compute_form_data(retrieved_data);
+        computed_data_array[row_index] = computed_data
+        update_record(computed_data, row);
+    }
+    row = reset_form();
+    console.log(array_of_form_data);
+}); 
+
+
+
 
